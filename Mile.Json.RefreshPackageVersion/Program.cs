cd68b03dcec12bffbe9ea86fc538abbf83fb26d6
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-25 09:01:45 +0800

    Create Mile.Json.RefreshPackageVersion project.

---
+++ Mile.Json.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.Json.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Mile.Json.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
