2926fdf4cb4f63d9a48dc834cc4bbbea003ca3ee
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-25 19:34:21 +0800

    Add Mile.Json wrapper.

---
+++ Mile.Json/Mile.Json.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Json.cpp
+ * PURPOSE:   Implementation for the Mile.Json
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mile.Json.h"
---
+++ Mile.Json/Mile.Json.h
@@ -0,0 +1,29 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Json.h
+ * PURPOSE:   Definition for the Mile.Json
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_JSON
+#define MILE_JSON
+
+#if (defined(__cplusplus) && __cplusplus >= 201703L)
+#elif (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
+#else
+#error "[Mile.Json] You should use a C++ compiler with the C++17 standard."
+#endif
+
+#include "Mile.Json.Implementation/json.hpp"
+
+namespace Mile::Json
+{
+
+}
+
+#endif // !MILE_JSON
