5620d3a85e7a3ebec3d639654eb1d950fcdd7411
MouriNaruto <Kenji.Mouri@outlook.com>
2025-07-28 01:14:57 +0800

    Fix the source code file header comment format.

--- Mile.Json/Mile.Json.cpp
+++ Mile.Json/Mile.Json.cpp
@@ -1,9 +1,9 @@
 ﻿/*
- * PROJECT:   Mouri Internal Library Essentials
- * FILE:      Mile.Json.cpp
- * PURPOSE:   Implementation for the Mile.Json
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Json.cpp
+ * PURPOSE:    Implementation for the Mile.Json
  *
- * LICENSE:   The MIT License
+ * LICENSE:    The MIT License
  *
  * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
  */
--- Mile.Json/Mile.Json.h
+++ Mile.Json/Mile.Json.h
@@ -1,9 +1,9 @@
 ﻿/*
- * PROJECT:   Mouri Internal Library Essentials
- * FILE:      Mile.Json.h
- * PURPOSE:   Definition for the Mile.Json
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Json.h
+ * PURPOSE:    Definition for the Mile.Json
  *
- * LICENSE:   The MIT License
+ * LICENSE:    The MIT License
  *
  * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
  */
